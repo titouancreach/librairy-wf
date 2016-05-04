9552ff7bf89c20e6b95060074b2451a3ca69fc81
Titouan CREACH <titouan.creach@gmail.com>
2016-05-04 16:10:53 +0000

    modify style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def get_status_from_number(n)
+        case n
+          when 0
+              "Inactive"
+          when 1
+              "On loan"
+          when 2
+              "Available"
+        end
+    end
 end
